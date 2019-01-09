4c3f935c252a922249dcd141dd509d722fee8cdd
Mihori <gabor.dobrozemszky@hotmail.com>
2019-01-09 14:42:14 +0100

    feat(Logo): add image

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Tilt from 'react-tilt'
+import brain from './brain.png';
 
 const Logo = () => {
   return (
     <div className="ma4 mt0">
       <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
-        <div className="Tilt-inner"> 👽 </div>
+        <div className="Tilt-inner pa3"><img style={{ paddingTop: '5px', height: '100px' }} src={brain} alt="logo" /></div>
       </Tilt>
     </div>
   )
