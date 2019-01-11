cd452ac26d9b313722ee4d3b3ddda4a6f59767fe
Mihori <gabor.dobrozemszky@hotmail.com>
2019-01-11 23:08:29 +0100

    feat(onButtonSubmit): add fetch /image endpoint

--- src/App.js
+++ src/App.js
@@ -35,7 +35,25 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (userData) => {
+    this.setState({ user: {
+      id: userData.id,
+      name: userData.name,
+      email: userData.email,
+      entries: userData.entries,
+      joined: userData.joined
+    }
+    });
   }
 
   calculateFaceLocation = (data) => {
@@ -62,7 +80,18 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            })
+          });
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
       .catch(err => console.log(err));
   }
 
@@ -91,7 +120,7 @@ class App extends Component {
         </div>
           : route === 'signin'
             ? <Signin onRouteChange={this.onRouteChange} />
-            : <Register onRouteChange={this.onRouteChange} />
+            : <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
         }
       </div>
     );
